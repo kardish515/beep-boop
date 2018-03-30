13087ec656a00aaf5cd109febaa7de237c9758f4
Nick Kardish <kardish515@gmail.com>
2018-03-30 09:31:36 -0700

    solve specs six and seven

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,15 @@ function beepBoop(number){
   var array = [];
   for (var i = 0; i <= counter; i++) {
     var n = i.toString();
-    if(n === "0"){
+    if(n === "3"){
+      array.push("I'm sorry, Dave. I'm afraid I can't do that.");
+    } else if(n === "1"){
+      array.push("Boop!");
+    } else if(n === "0"){
       array.push("Beep!");
     } else {
         array.push(n);
-      }
+    }
   }
   // if(number === "0"){
   //   return "Beep!";
