a647892c38c2980ae7926e218c6fef5729c74f4a
Nick Kardish <kardish515@gmail.com>
2018-03-30 10:16:07 -0700

    add css styling and image

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
-function beepBoop(number){
+function beepBoop(number, name){
   var counter = parseInt(number);
   var array = [];
   for (var i = 0; i <= counter; i++) {
     var n = i.toString();
     if(i%3 === 0 && i !== 0){
-      array.push("I'm sorry, Dave. I'm afraid I can't do that.");
+      array.push("I'm sorry, " + name + ". I'm afraid I can't do that.");
     } else if(n.includes("1")){
       array.push("Boop!");
     } else if(n.includes("0")){
@@ -20,7 +20,9 @@ $(document).ready(function(){
   $("#formOne").submit(function(event){
     event.preventDefault();
     var number = $("input#number").val();
-    var result = beepBoop(number);
+    var name = $("input#name").val();
+    var result = beepBoop(number, name);
     $("#output").text(result);
+    $(".output").show();
   });
 });
